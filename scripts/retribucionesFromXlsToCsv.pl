28dd37ab0cad92045bea5c5f167d7b35ad4b4dc0
oskyar <oskyar@gmail.com>
2014-04-09 02:08:35 +0200

    Ya descarga todos los archivos .xls de la página

--- scripts/retribucionesFromXlsToCsv.pl
+++ scripts/retribucionesFromXlsToCsv.pl
@@ -2,14 +2,15 @@
 
 #Versión 1 - Descarga los archivos .xls de la página, los pasa a csv y divide las tablas.
 
-#Para ejecutar este programa hay que instalar las librerías, JSON, Slurp, Switch y Simple
+#Para ejecutar este programa hay que instalar las librerías, JSON, File::Slurp, LWP::Simple, URI::http
+#Módulo para pasar de xls a csv http://search.cpan.org/~ken/xls2csv/script/xls2csv
 
 use File::Slurp qw(read_file);
 use File::Path qw(mkpath);
 use LWP::Simple;
 use Web::Scraper;
-
-
+use Spreadsheet::ParseExcel qw(worksheet );
+use Spreadsheet::ParseExcel::Workbook qw(get_name);
 use v5.014;
 
 use strict;
@@ -22,9 +23,10 @@ my $linea;
 my $matriculacion;
 use constant false => 0;
 use constant true  => 1;
-my %urls;
-
-
+my %hashUrls;
+my $content;
+my $parser;
+my $workbook;
 
 #Programa
 principal();
@@ -32,28 +34,44 @@ principal();
 
 #Métodos
 sub principal {
+    $parser  = Spreadsheet::ParseExcel->new();
+
     # URL para hacer scraping    
     my $urlToScrape = "http://gerencia.ugr.es/habilitacion/pages/legislacion/tabla_retribuciones";
     # Preparamos los datos
     my $teamsdata = scraper {
-        # Guardaremos el enlace de las url
+        # Guardaremos el enlace de las url que tengan esta estructura
         process "div#contenido > div > ul > li > div > a", 'urls[]' => '@href';
-        # Guardaremos el texto que hay en las URL
+        # Guardaremos el texto que hay en las URL dentro de esta estructura
         process "div#contenido > div > ul > li > div > a", 'teams[]' => 'TEXT';
     };
 
     # "Scrapeando" los datos
-    my $res = $teamsdata->scrape(URI->new($urlToScrape));  
+    my $res = $teamsdata->scrape(URI->new($urlToScrape));
 
-    # Hash con el texto que acompaña a la URL y la URL {texto, url}
-    for my $i (0 .. $#{$res->{teams}}) {
-        $urls{$res->{teams}[$i]} = $res->{urls}[$i];
-#        say $res->{teams}[$i];
-#        say $res->{urls}[$i];
-#        print "\n";
-     #   print FILE $res->{teams}[$i];
-     #   print FILE "\n";
+    for my $i (0 .. $#{$res->{'teams'}}) {
+        # Hash con el texto que acompaña a la URL y la URL {texto, url}
+        my $name = $res->{'teams'}[$i];
+	    $name =~ s/(^ )|( $)//g;
+        #my $url = $res->{'urls'}[$i];
+        #$hashUrls{$name} = $url;
+        print readlink($res->{'urls'}[$i]);
+        # Me descargo los archivos de los enlaces que se han guardado.
+        my $code = getstore($res->{urls}[$i],"$name.xls");
+        $workbook = $parser->parse("$name.xls");
+        if ( !defined $workbook ){
+            print "ERROR: ".$parser->error(), " \[$name\]\n";
+        }else{
+            splitTables("$name.xls", $workbook);            
+        }
     }
+
+
+    #Para imprimir el hash en caso de necesitarlo
+    #while ((my $texto,my $url) = each %hashUrls){
+    #    print "$texto = $url\n";
+    #}
+
 
     #open(O, ">out26.txt");
     #my $content = get($url);
@@ -61,6 +79,9 @@ sub principal {
 
     #say O $content;
 
+}
 
-     
+sub splitTables{
+    xls2csv -x "1252spreadsheet.xls" -b WINDOWS-1252 -c "ut8csvfile.csv" -a UTF-8;
+    
 }
