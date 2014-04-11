e444816e57293dca3eb7ccb273238865bcdc98b7
oskyar <oskyar@gmail.com>
2014-04-11 11:55:06 +0200

    Añadida Licencia GPLv3 a todos los script

--- scripts/deleteLine.pl
+++ scripts/deleteLine.pl
@@ -1,6 +1,23 @@
+#!/usr/bin/perl
+
+#       CopyRight 2014 Óscar Zafra (oskyar@gmail.com)
+#
+#       This program is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #!/usr/bin/env perl
 
-#Versión 1 - Elimina una linea/s o columna/s que quieras de un archivo CSV.
+#Versión 1 - Elimina linea/s o columna/s que quieras de un archivo CSV.
 
 # Para ejecutar este programa hay que instalar las librerías: File::Slurp, LWP::Simple, Text::CSV
 # Script que elimina o una fila o una columna de un archivo CSV
@@ -15,6 +32,7 @@ use open 'locale';
 my @inFiles;
 my $line;
 my $numLine;
+
 #Programa
 principal();
 
