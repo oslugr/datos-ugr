ad1a5e870e4f637691a482f5c80c5a82a7a84dfc
oskyar <oskyar@gmail.com>
2014-04-23 10:12:55 +0200

    Se eliminan datos importantes y se añaden licencias GPLv3 a los script

--- scripts/deleteLine.pl
+++ scripts/deleteLine.pl
@@ -1,6 +1,23 @@
+#!/usr/bin/perl
+
+#       CopyRight 2014 Óscar Zafra (oskyar@gmail.com)
+#
+#       This program is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #!/usr/bin/env perl
 
-#Versión 1 - Elimina una linea/s o columna/s que quieras de un archivo CSV.
+#Versión 1.1 - Elimina linea/s o columna/s que quieras de uno o varios archivos CSV.
 
 # Para ejecutar este programa hay que instalar las librerías: File::Slurp, LWP::Simple, Text::CSV
 # Script que elimina o una fila o una columna de un archivo CSV
@@ -12,6 +29,10 @@ use strict;
 use utf8;
 use open 'locale'; 
 
+my @inFiles;
+my $line;
+my $numLine;
+
 #Programa
 principal();
 
@@ -19,9 +40,34 @@ principal();
 #Métodos
 sub principal {
 
-    if( scalar(@ARGV) > 2 ){
-        my $in = shift @ARGV;
-        my $line = shift @ARGV;
+    #Se guardan los argumentos que empiezan por csv en un array
+    if(scalar(@ARGV) > 2){
+        while ($ARGV[0] =~ /([\w\d]*\.[cC][sS][vV]$)/){
+            push @inFiles, shift @ARGV;
+        }
+        if(scalar(@ARGV) >= 2){
+            $line = shift @ARGV;
+            $numLine = int(shift @ARGV);
+        }else{
+            if(scalar(@ARGV)!=0){
+                print "ERROR: no ha introducido los parámetros correctos\n";
+                error();
+                exit(0);
+            }
+        }
+        if(scalar(@inFiles) == 0){
+            print "ERROR: No ha introducido ningún argumento de tipo archivo.csv\n";
+            error();
+            exit(0);
+        }
+    }else{
+        print "\n$0\nElimina la línea ó columna de los archivos .csv que se indiquen \n";
+        print "FORMATO: " . $0 . " archivo.csv... <fil|col> numberLine... \n\n";
+        error();
+        exit(0);
+    }
+    
+    foreach my $in (@inFiles){
         # Creamos instancia de CSV
         my $csv = Text::CSV->new ( { binary => 1, 
             quote_char          => '"',
@@ -51,9 +97,7 @@ sub principal {
             my @sortedNumLines = sort {$a >= $b}@ARGV;
             if( "fil" eq $line){
                 my $cont=0;
-                my $numLine = 0;
 
-                $numLine = int(shift @ARGV);
                 while (my $row = $csv->getline( $inHandler )){
                     #Se le resta 1 al número de linea
                     if($cont != ($numLine -1)){
@@ -76,14 +120,11 @@ sub principal {
         }
         close($outHandler);
         close($inHandler);
-    }else{
-        say "ERROR: Número de argumentos inválido\n";
-        error();
     }
 }
 
 sub error{
-    say "Ejecuta: perl deleteLine.pl archivo.csv [fil|col] (line/s)";
+    say "Ejecuta: perl deleteLine.pl archivo.csv... <fil|col> numberLine...";
     say "|--Ejemplo: perl deleteLine.pl archivo.csv fil 1 4 5";
     say "|--Ejecución: Borrará las filas 1, 4 y 5 de archivo.csv";
 }
