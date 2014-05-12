7b00abc2d1d0ba837c023bcb7b873465b966786a
oskyar <oskyar@gmail.com>
2014-05-12 13:38:41 +0200

    Ahora se muestra por terminal al crear un archivo, su nombre con color verde y la barra de progreso en blanco

--- scripts/retribucionesFromXlsToCsv.pl
+++ scripts/retribucionesFromXlsToCsv.pl
@@ -26,6 +26,7 @@ use LWP::Simple;
 use Web::Scraper;
 use MIME::Types qw();
 use Smart::Comments '###';
+use Term::ANSIColor;
 use Spreadsheet::ParseExcel qw(worksheet );
 use Spreadsheet::ParseExcel::Workbook qw(get_name);
 use v5.014;
@@ -40,7 +41,8 @@ my $mimetypes = MIME::Types->new();
 my @variables = LoadFile('./retribucionesFromXlsToCsv.yml');
 
 #Esto debe de ser un parámetro pasado al programa.
-my $type = $mimetypes->mimeTypeOf('xls');
+my $format = "xls";
+my $type = $mimetypes->mimeTypeOf($format);
 
 # URL para hacer scraping    
 my $url_to_scrape = $variables[0]->{'urlToScrape2'};
@@ -55,32 +57,39 @@ my $teamsdata = scraper {
 
 # "Scrapeando" los datos
 my $res = $teamsdata->scrape(URI->new($url_to_scrape));
+
+#Creamos la carpeta donde se almacenarán los archivos exportados
 mkpath("csv");
-for my $i (0 .. $#{$res->{'teams'}}) { ### Procesando [===|    ] % Terminado
-    # Hash con el texto que acompaña a la URL y la URL {texto, url}
-    my $name = $res->{'teams'}[$i];
-    $name =~ s/(^ )|( $)//g;
-    # Me descargo los archivos de los enlaces que se han guardado.
-    my @info = head($res->{urls}[$i]);
-    #say $info[0];
-    if($info[0] =~ /$type/){
-	    my $code = getstore($res->{urls}[$i],"$name.xls");
-	    my $workbook = $parser->parse("$name.xls");
-	    if ( !defined $workbook ){
-	      print "ERROR: ".$parser->error(), " \[$name\]\n";
-	    }else{
-	      print "\n\n$name.xls\n\n";
-	      xls2csv("$name", $workbook);
-	    }
+#Procesamos todas las URL de la página comprobando su cabecera.
+for my $i (0 .. $#{$res->{'teams'}}) { ### Procesando [===|         ] % Terminado
+    my @info = head($res->{urls}[$i]);  
+    #Comparo MIME del archivo y si es del tipo que quiero, paso a descargar y procesar.
+    if($info[0] =~ m/$type/){
+        # Hash con el texto que acompaña a la URL y la URL {texto, url}
+        my $name = $res->{'teams'}[$i];
+        $name =~ s/(^ )|( $)//g;
+        # Me descargo los archivos de los enlaces que se han guardado.
+        my $code = getstore($res->{urls}[$i],"$name.$format");
+        my $workbook = $parser->parse("$name.$format");
+        if ( !defined $workbook ){
+            print "ERROR: ".$parser->error(), " \[$name\]\n";
+        }else{
+             #print "\n\n$name.xls\n\n";
+             xls2csv("$name", $workbook);
+        }
     }
 }
 
-
 sub xls2csv{
-    my $command = " xls2csv -x \"$_[0].xls\" -b ISO-8859-1 -c csv/\"$_[0].csv\" -a ISO-8859-1 -f";
+    my $command = " xls2csv -x \"$_[0].$format\" -b ISO-8859-1 -c csv/\"$_[0].csv\" -a ISO-8859-1 -f -q";
+    
+    ## Archivo creado: $_[0].".csv"
+    print color 'bold green';
+    print ("\nArchivo creado: $_[0].csv\n");
+    print color 'reset';
     system($command);
     if ($?) {
-      print "[ERROR] command failed: $!\n";
-      print "[ERROR] No se ha podido ejecutar el comando para la entrada \"$1\" y la salida: \"$1.csv\"\n";
+        print "[ERROR] command failed: $!\n";
+        print "[ERROR] No se ha podido ejecutar el comando para la entrada \"$_[0].$format\" y la salida: \"$_[0].csv\"\n";
     }
 }
