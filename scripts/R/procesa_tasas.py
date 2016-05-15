2dacca04fee3c814a7c826cf88655948d682e1f0
German Martinez <germaaan@gmail.com>
2016-05-15 23:12:41 +0200

    Script procesamiento completado

--- scripts/R/procesa_tasas.py
+++ scripts/R/procesa_tasas.py
@@ -8,27 +8,64 @@ sys.setdefaultencoding('utf-8')
 
 import csv
 
-with open('tasa_rendimiento.csv', 'r') as csvin:
-	reader = csv.reader(csvin, delimiter=',', quotechar='"')
+def processing(infile, outfile):
+	with open(infile, 'r') as csvin:
+		reader = csv.reader(csvin, delimiter=',', quotechar='"')
+		reader.next()
 
-	codigo = aux = year = -1;
-	lst = []
-	for row in reader:
-		#print ', '.join(row)
-		if row[0] != codigo:
-			aux += 1
-			codigo = row[0]
-			lst.append([])
-			lst[aux].append(row[2])
-			lst[aux].append(row[3])
-		else:
-			lst[aux].append(row[3])
+		codigo = aux = year = -1;
+		lst = []
+		for row in reader:
+			if row[0] != codigo:
+				aux += 1
+				codigo = row[0]
+				lst.append([])
+				lst[aux].append(row[2])
+				year = int(row[1])			
+
+				if year == 2011:
+					lst[aux].append(row[3])
+				elif year == 2012:
+					lst[aux].append('')
+					lst[aux].append(row[3])
+				elif year == 2013:
+					lst[aux].append('')
+					lst[aux].append('')
+					lst[aux].append(row[3])
+				elif year == 2014:
+					lst[aux].append('')
+					lst[aux].append('')
+					lst[aux].append('')
+					lst[aux].append(row[3])
+				elif year == 2015:
+					lst[aux].append('')
+					lst[aux].append('')
+					lst[aux].append('')
+					lst[aux].append('')
+					lst[aux].append(row[3])
+			else:
+				if int(row[1]) == year+1:
+					lst[aux].append(row[3])
+					year = int(row[1])
+				elif int(row[1]) == year+2:
+					lst[aux].append('')
+					lst[aux].append(row[3])
+					year = int(row[1])
+				elif int(row[1]) == year+3:
+					lst[aux].append('')
+					lst[aux].append('')
+					lst[aux].append(row[3])
+					year = int(row[1])
+				elif int(row[1]) == year+4:
+					lst[aux].append('')
+					lst[aux].append('')
+					lst[aux].append('')
+					lst[aux].append('')
+					year = int(row[1])
 			
-		 # print row
-	# print aux
-	# print lst
-	with open('prueba.csv', 'w') as csvout:
-		writer = csv.writer(csvout, delimiter=',', quotechar='"')
-		for row in lst:
-			writer.writerow(row)
-	
+		with open(outfile, 'w') as csvout:
+			writer = csv.writer(csvout, delimiter=',', quotechar='"')
+			for row in lst:
+				writer.writerow(row)
+
+processing('tasa_rendimiento.csv', 'tasa_rendimiento_agrupadas.csv')
