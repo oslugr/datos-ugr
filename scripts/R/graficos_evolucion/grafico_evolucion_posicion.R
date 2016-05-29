40288b36c688b0d750ce339b32a9efa1dd99e109
German Martinez <germaaan@gmail.com>
2016-05-30 01:28:58 +0200

    Cambios final a caso básico

--- scripts/R/graficos_evolucion/grafico_evolucion_posicion.R
+++ scripts/R/graficos_evolucion/grafico_evolucion_posicion.R
@@ -38,6 +38,8 @@ datos_rendimiento_artes$ANIO[datos_rendimiento_artes$ANIO=="X2014"] <- 2014
 datos_rendimiento_artes$ANIO[datos_rendimiento_artes$ANIO=="X2015"] <- 2015
 datos_rendimiento_artes$ANIO <- factor(datos_rendimiento_artes$ANIO)
 datos_rendimiento_sociales <- melt(datos_rendimiento_sociales, id=c("TITULO"))
+
+
 names(datos_rendimiento_sociales)[2]<-"ANIO"
 names(datos_rendimiento_sociales)[3]<-"VALOR"
 datos_rendimiento_sociales$VALOR <- as.numeric(chartr(",", ".", datos_rendimiento_sociales$VALOR))
@@ -49,52 +51,14 @@ datos_rendimiento_sociales$ANIO[datos_rendimiento_sociales$ANIO=="X2014"] <- 201
 datos_rendimiento_sociales$ANIO[datos_rendimiento_sociales$ANIO=="X2015"] <- 2015
 datos_rendimiento_sociales$ANIO <- factor(datos_rendimiento_sociales$ANIO)
 
-
-for (titulo in datos_rendimiento_artes$TITULO){
-  indice <- seq.int(length(titulos))
-  
-  if (titulo == titulos[indice])
-    datos_rendimiento_artes$GROSOR <- grosor[indice]
-}
-for (titulo in datos_rendimiento_sociales$TITULO){
-  indice <- seq.int(length(titulos))
-  
-  if (titulo == titulos[indice])
-    datos_rendimiento_sociales$GROSOR <- grosor[indice]
-}
-
-datos_rendimiento_artes$GROSOR <- as.numeric(datos_rendimiento_artes$GROSOR)
-grosor <- as.numeric(grosor)
-datos_rendimiento_sociales$GROSOR <- as.numeric(datos_rendimiento_sociales$GROSOR)
-grosor <- as.numeric(grosor)
-
-
-ggplot(datos_rendimiento_artes, aes(x=ANIO, y=VALOR, group=TITULO, colour=TITULO)) + 
-  geom_point() + geom_line() + xlab("AÑO") + ylab("POSICION") +
-  ggtitle("EVOLUCIÓN TASA DE RENDIMIENTO EN ARTES Y HUMANIDADES") + 
-  scale_y_reverse(expand=c(0.01, 0.01), limits=c(filas_artes, 1), breaks=seq(1, filas_artes, 1), labels=titulos) +
-  scale_x_discrete(expand=c(0.05, 0.05)) +
-  theme(plot.title=element_text(family="Lucida Bright", face="bold", size=20), 
-        axis.title=element_text(size=15), axis.text.x=element_text(family="Lucida Bright"), 
-        axis.text.y=element_text(size=10), legend.position="none", 
-        plot.margin = unit(c(0.2, 0.2, 0.2, 0.2), "in"))
-
-
-ggplot(datos_rendimiento_artes, aes(x=ANIO, y=VALOR, group=TITULO, colour=TITULO)) + 
-  geom_point() + geom_line() + xlab("AÑO") + ylab("POSICION") +
-  ggtitle("EVOLUCIÓN TASA DE RENDIMIENTO EN ARTES Y HUMANIDADES") + 
-  scale_y_reverse(expand=c(0.01, 0.01), limits=c(filas_artes, 1), breaks=seq(1, filas_artes, 1), labels=titulos) +
-  scale_x_discrete(expand=c(0.05, 0.05)) +
-  theme(plot.title=element_text(family="Lucida Bright", face="bold", size=20), 
-        axis.title=element_text(size=15), axis.text.x=element_text(family="Lucida Bright"), 
-        axis.text.y=element_text(size=10), legend.position="none", 
-        plot.margin = unit(c(0.2, 0.2, 0.2, 0.2), "in"))
 ggplot(datos_rendimiento_sociales, aes(x=ANIO, y=VALOR, group=TITULO, colour=TITULO)) + 
   geom_point() + geom_line() + xlab("AÑO") + ylab("POSICION") +
   ggtitle("EVOLUCIÓN TASA DE RENDIMIENTO EN CIENCIAS SOCIALES Y JURÍDICAS") + 
-  scale_y_reverse(expand=c(0.01, 0.01), limits=c(filas_sociales, 1), breaks=seq(1, filas_sociales, 1), labels=titulos) +
-  scale_x_discrete(expand=c(0.05, 0.05)) +
+  scale_y_reverse(expand=c(0.01, 0.01), limits=c(filas_sociales, 1), breaks=seq(1, filas_sociales, 1), 
+                  labels=titulos) +
+  scale_x_discrete(expand=c(0.02, 0.02)) +
   theme(plot.title=element_text(family="Lucida Bright", face="bold", size=20), 
         axis.title=element_text(size=15), axis.text.x=element_text(family="Lucida Bright"), 
         axis.text.y=element_text(size=10), legend.position="none", 
         plot.margin = unit(c(0.2, 0.2, 0.2, 0.2), "in"))
+
