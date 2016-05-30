a709d2f3f4a78b5176f89ea039c06ff36ce8d1d3
German Martinez <germaaan@gmail.com>
2016-05-30 02:59:09 +0200

    Añade abandono inicial

--- scripts/R/graficos_evolucion/grafico_evolucion.R
+++ scripts/R/graficos_evolucion/grafico_evolucion.R
@@ -6,7 +6,7 @@ library("ggplot2")
 library("reshape2")
 
 # Función para hacer gráfico genérico
-grafico <- function(datos_tasa, area, tasa){
+grafico <- function(datos_tasa, area, tasa, completo){
   datos <- datos_tasa[datos_tasa[, "AREA"] == area,]
   orden <- as.character(factor(datos$TITULO))
   filas <- nrow(datos)
@@ -21,16 +21,22 @@ grafico <- function(datos_tasa, area, tasa){
   names(datos)[3]<-"VALOR"
   datos$VALOR <- as.numeric(chartr(",", ".", datos$VALOR))
   datos$ANIO <- as.character(datos$ANIO)
-  datos$ANIO[datos$ANIO=="X2011"] <- 2011
-  datos$ANIO[datos$ANIO=="X2012"] <- 2012
-  datos$ANIO[datos$ANIO=="X2013"] <- 2013
-  datos$ANIO[datos$ANIO=="X2014"] <- 2014
-  datos$ANIO[datos$ANIO=="X2015"] <- 2015
+  if (completo == TRUE){
+    datos$ANIO[datos$ANIO=="X2011"] <- 2011
+    datos$ANIO[datos$ANIO=="X2012"] <- 2012
+    datos$ANIO[datos$ANIO=="X2013"] <- 2013
+    datos$ANIO[datos$ANIO=="X2014"] <- 2014
+    datos$ANIO[datos$ANIO=="X2015"] <- 2015
+  } else {
+    datos$ANIO[datos$ANIO=="X2013"] <- 2013
+    datos$ANIO[datos$ANIO=="X2014"] <- 2014
+    datos$ANIO[datos$ANIO=="X2015"] <- 2015
+  }
   datos$ANIO <- factor(datos$ANIO)
   
   ggplot(datos, aes(x=ANIO, y=VALOR, group=TITULO, colour=TITULO)) + 
     geom_point() + geom_line() + xlab("AÑO") + ylab("POSICION") +
-    ggtitle(paste("EVOLUCIÓN TASA DE", tasa, "EN", area, sep=" ")) + 
+    ggtitle(paste("EVOLUCIÓN DE", tasa, "EN", area, sep=" ")) + 
     scale_y_reverse(expand=c(0.01, 0.01), limits=c(filas, 1), breaks=seq(1, filas, 1), 
                     labels=titulos) +
     scale_x_discrete(expand=c(0.02, 0.02)) +
@@ -44,47 +50,69 @@ grafico <- function(datos_tasa, area, tasa){
 # Archivos de datos
 tasa_rendimiento <- paste(getwd(), "/tasa_rendimiento_posicion.csv", sep="")
 tasa_exito <- paste(getwd(), "/tasa_exito_posicion.csv", sep="")
+tasa_abandono_inicial <- paste(getwd(), "/tasa_abandono_inicial_posicion.csv", sep="")
 datos_rendimiento <- read.csv(file=tasa_rendimiento, header=TRUE, fileEncoding="iso-8859-1", sep=",")
 datos_exito <- read.csv(file=tasa_exito, header=TRUE, fileEncoding="iso-8859-1", sep=",")
+datos_abandono_inicial <- read.csv(file=tasa_abandono_inicial, header=TRUE, fileEncoding="iso-8859-1", sep=",")
 
 if (!dir.exists("graph")) dir.create("graph", showWarnings = TRUE, recursive = FALSE, mode = "0777")
 
 png("graph/evolucion_rendimiento_artes.png", width=1221, height=1000, units='px')
-grafico(datos_rendimiento, "ARTES Y HUMANIDADES", "RENDIMIENTO")
+grafico(datos_rendimiento, "ARTES Y HUMANIDADES", "RENDIMIENTO", TRUE)
 dev.off()
 
 png("graph/evolucion_rendimiento_ciencias.png", width=1221, height=1000, units='px')
-grafico(datos_rendimiento, "CIENCIAS", "RENDIMIENTO")
+grafico(datos_rendimiento, "CIENCIAS", "RENDIMIENTO", TRUE)
 dev.off()
 
 png("graph/evolucion_rendimiento_salud.png", width=1221, height=1000, units='px')
-grafico(datos_rendimiento, "CIENCIAS DE LA SALUD", "RENDIMIENTO")
+grafico(datos_rendimiento, "CIENCIAS DE LA SALUD", "RENDIMIENTO", TRUE)
 dev.off()
 
 png("graph/evolucion_rendimiento_sociales.png", width=1221, height=1000, units='px')
-grafico(datos_rendimiento, "CIENCIAS SOCIALES Y JURÍDICAS", "RENDIMIENTO")
+grafico(datos_rendimiento, "CIENCIAS SOCIALES Y JURÍDICAS", "RENDIMIENTO", TRUE)
 dev.off()
 
 png("graph/evolucion_rendimiento_ingenieria.png", width=1221, height=1000, units='px')
-grafico(datos_rendimiento, "INGENIERÍA Y ARQUITECTURA", "RENDIMIENTO")
+grafico(datos_rendimiento, "INGENIERÍA Y ARQUITECTURA", "RENDIMIENTO", TRUE)
 dev.off()
 
 png("graph/evolucion_exito_artes.png", width=1221, height=1000, units='px')
-grafico(datos_exito, "ARTES Y HUMANIDADES", "EXITO")
+grafico(datos_exito, "ARTES Y HUMANIDADES", "EXITO", TRUE)
 dev.off()
 
 png("graph/evolucion_exito_ciencias.png", width=1221, height=1000, units='px')
-grafico(datos_exito, "CIENCIAS", "EXITO")
+grafico(datos_exito, "CIENCIAS", "EXITO", TRUE)
 dev.off()
 
 png("graph/evolucion_exito_salud.png", width=1221, height=1000, units='px')
-grafico(datos_exito, "CIENCIAS DE LA SALUD", "EXITO")
+grafico(datos_exito, "CIENCIAS DE LA SALUD", "EXITO", TRUE)
 dev.off()
 
 png("graph/evolucion_exito_sociales.png", width=1221, height=1000, units='px')
-grafico(datos_exito, "CIENCIAS SOCIALES Y JURÍDICAS", "EXITO")
+grafico(datos_exito, "CIENCIAS SOCIALES Y JURÍDICAS", "EXITO", TRUE)
 dev.off()
 
 png("graph/evolucion_exito_ingenieria.png", width=1221, height=1000, units='px')
-grafico(datos_exito, "INGENIERÍA Y ARQUITECTURA", "EXITO")
+grafico(datos_exito, "INGENIERÍA Y ARQUITECTURA", "EXITO", TRUE)
+dev.off()
+
+png("graph/evolucion_abandono_inicial_artes.png", width=1221, height=1000, units='px')
+grafico(datos_abandono_inicial, "ARTES Y HUMANIDADES", "ABANDONO INICIAL", FALSE)
+dev.off()
+
+png("graph/evolucion_abandono_inicial_ciencias.png", width=1221, height=1000, units='px')
+grafico(datos_abandono_inicial, "CIENCIAS", "ABANDONO INICIAL", FALSE)
+dev.off()
+
+png("graph/evolucion_abandono_inicial_salud.png", width=1221, height=1000, units='px')
+grafico(datos_abandono_inicial, "CIENCIAS DE LA SALUD", "ABANDONO INICIAL", FALSE)
+dev.off()
+
+png("graph/evolucion_abandono_inicial_sociales.png", width=1221, height=1000, units='px')
+grafico(datos_abandono_inicial, "CIENCIAS SOCIALES Y JURÍDICAS", "ABANDONO INICIAL", FALSE)
+dev.off()
+
+png("graph/evolucion_abandono_inicial_ingenieria.png", width=1221, height=1000, units='px')
+grafico(datos_abandono_inicial, "INGENIERÍA Y ARQUITECTURA", "ABANDONO INICIAL", FALSE)
 dev.off()
